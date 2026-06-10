5ddc282203222564eb0c31b400a50ddb9a09bc99
Daylfly <dazzlenolik@gmail.com>
2026-06-10 13:58:07 +0300

    fix styles and node version

--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
+    basePath: "/MIND",    // Указывает правильный путь для страниц
+    assetPrefix: "/MIND/", // ОБЯЗАТЕЛЬНО: Указывает правильный путь для стилей и скриптов
 
     images: {
-            unoptimized: true,
-            remotePatterns: [
-                {
-                    protocol: "https",
-                    hostname: "i.ytimg.com",
-                    pathname: "/**",
-                },
-                {
-                    protocol: "https",
-                    hostname: "images.unsplash.com",
-                    pathname: "/**",
-                },
-            ],
-        },
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "://ytimg.com",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "://unsplash.com",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
-
